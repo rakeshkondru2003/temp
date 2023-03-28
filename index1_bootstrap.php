bf7b3be9ae11fa1fd8fb5a51cb2efe4763132348
rakeshkondru2003 <129033221+rakeshkondru2003@users.noreply.github.com>
2023-03-28 15:38:51 +0530

    Add files via upload

---
+++ index1_bootstrap.php
@@ -0,0 +1,44 @@
+?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Rakesh</h1> 
+</div>
+  
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>king</h3>
+      <img src="https://crictoday.com/wp-content/uploads/2023/01/virat-kohli-1671970326.jpg",alt="img"height="250" width="400">
+      <p>kohli is the former captain of india</p>
+      <p>he is the good batsman</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>rohit</h3>
+      <img src="https://www.india.com/wp-content/uploads/2022/09/rohit-sharma.png"alt="img"height="250" width="400">
+      <p>rohit is the captain of india</p>
+      <p>he is a good hiter</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>aswin</h3> 
+      <img src="https://1847884116.rsc.cdn77.org/tamil/home/bowling140323_3.jpg",alt="img" height="250" widht="250">     
+      <p>aswin is the good spinner</p>
+      <p>aswin is a average batsman</p>
+
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+?>
